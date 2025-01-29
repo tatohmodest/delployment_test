2c60a6114c78090af6c1b69a20eecc4d1583ac7d
Tatoh Modest <tatohmodest525@gmail.com>
2025-01-29 10:06:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+let b = "number"
+
+
+//checking if values a strings 
+// if (b === 'string'){
+//     console.log(`Hey ${b} is a String`)
+// }
+// else if(b === "number"){
+
+//     console.log("hey the value is a number ", b)
+// }
+
+// else{
+//     console.log("The value is not a String, instead it it a ", b )
+// }
+
+// switch (b) {
+//     case "string":
+//         console.log("This is a string")
+//         break;
+//     case "number":
+//         console.log("this is a number")
+//         break;
+//     case "boolean":
+//         console.log("this is a boolean")
+//         break;
+//     default:
+//         console.log("It is not a number, string , or boolean")
+
+// }
+
+let a = "yam"
+
+let answer = a == "yam" ?  "The answer is true"    : "the answer is false";
+
+console.l
